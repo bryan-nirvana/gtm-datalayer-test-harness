77625175daf8f93a49b07201cf804ee53be93316
Bryan Godwin <bryan@nirvana.tech>
2022-08-28 05:52:48 -0500

    added fake user

---
+++ fake-user.js
@@ -0,0 +1,28 @@
+import { faker } from "https://cdn.skypack.dev/@faker-js/faker";
+
+const getFakeUser = () => {
+  let id = faker.datatype.uuid();
+  let firstName = faker.name.firstName();
+  let lastName = faker.name.lastName();
+  let fullName = firstName + " " + lastName;
+  let email = faker.internet.email(firstName, lastName);
+  let phone = faker.phone.number("+1-###-###-####");
+  let deviceId = faker.datatype.uuid();
+
+  let user = {
+    nirvanaUserId: id,
+    nirvanaDeviceId: deviceId,
+    nirvanaUserStatus: "applicant",
+    firstName: firstName,
+    lastName: lastName,
+    fullName: fullName,
+    email: email,
+    phone: phone,
+  };
+
+  console.table(user);
+
+  return user;
+};
+
+window.getFakeUser = getFakeUser;
