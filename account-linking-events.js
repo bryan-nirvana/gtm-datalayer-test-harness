e8a69e574ef8ceb44d8193419b94abe7f86862ee
Bryan Godwin <bryan@nirvana.tech>
2022-09-16 08:00:30 -0500

    added braze events

--- account-linking-events.js
+++ account-linking-events.js
@@ -1,3 +1,74 @@
+const linkAccountIntent = () => {
+  let button = document.getElementById("link-account-intent");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    let event = {};
+    event.event = "linkAccountIntent";
+    event.category = "accountLinking";
+    event.subCategory = "linkDecision";
+    event.eventType = "eventTracking";
+
+    pushEventToDataLayer(event);
+  });
+};
+
+const skipForNowIntentLinking = () => {
+  let button = document.getElementById("skip-for-now-intent-linking");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    let event = {};
+    event.event = "skipForNowIntent";
+    event.category = "accountLinking";
+    event.subCategory = "linkDecision";
+    event.eventType = "eventTracking";
+
+    pushEventToDataLayer(event);
+  });
+};
+
+const firstDeposit = () => {
+  let button = document.getElementById("first-deposit");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    let event = {};
+    event.event = "firstDeposit";
+    event.category = "accountLinking";
+    event.subCategory = "firstDeposit";
+    event.eventType = "eventTracking";
+    event.amount = 50.75;
+
+    pushEventToDataLayer(event);
+  });
+};
+
+const skipForNowIntentDeposit = () => {
+  let button = document.getElementById("skip-for-now-intent-deposit");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    let event = {};
+    event.event = "skipForNowIntent";
+    event.category = "accountLinking";
+    event.subCategory = "firstDeposit";
+    event.eventType = "eventTracking";
+
+    pushEventToDataLayer(event);
+  });
+};
+
+/// these are plaid linking events
+
+/// OPEN
+
+/// TRANSITION_VIEW
+
+/// SELECT_INSTITUTION
+
+/// SUBMIT_CREDENTIALS
+
+/// HANDOFF
+
+/// ERROR
+
 (() => {
   document.addEventListener("DOMContentLoaded", () => {
     console.log(
---
+++ braze.js
@@ -0,0 +1,97 @@
+// billboard campaign triggers 1-10
+
+const billboardEvents = () => {
+  let button = document.getElementById("braze-billboard");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    for (let i = i; i < 11; i++) {
+      pushEventToDataLayer({
+        event: "campaignTrigger",
+        category: "braze",
+        subCategory: "billboard",
+        eventType: "eventTracking",
+      });
+    }
+  });
+};
+
+// experiment trigger (user needs to input an experiment Id for this)
+const experimentTriggerVar1 = () => {
+  let button = document.getElementById("braze-experiment-trigger-var-1");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    pushEventToDataLayer({
+      event: "campaignTrigger",
+      category: "braze",
+      subCategory: "experiment",
+      eventType: "eventTracking",
+      experimentId: "FdedfcvFRxyZ_syoHheFeA",
+      experimentVariantId: "1",
+    });
+  });
+};
+
+// experiment trigger (user needs to input an experiment Id for this)
+const experimentTriggerVar2 = () => {
+  let button = document.getElementById("braze-experiment-trigger-var-2");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    pushEventToDataLayer({
+      event: "campaignTrigger",
+      category: "braze",
+      subCategory: "experiment",
+      eventType: "eventTracking",
+      experimentId: "FdedfcvFRxyZ_syoHheFeA",
+      experimentVariantId: "2",
+    });
+  });
+};
+
+// experimentStarted event
+// experiment trigger (user needs to input an experiment Id for this)
+const experimentStartedVar1 = () => {
+  let button = document.getElementById("braze-experiment-started-var-1");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    pushEventToDataLayer({
+      event: "experimentStarted",
+      category: "experiments",
+      subCategory: "googleOptimize",
+      eventType: "eventTracking",
+      experimentId: "FdedfcvFRxyZ_syoHheFeA",
+      experimentVariantId: "1",
+      experimentName: "QA Test",
+      experimentVariantName: "Pinwheel",
+    });
+  });
+};
+
+const experimentStartedVar2 = () => {
+  let button = document.getElementById("braze-experiment-started-var-2");
+  button.style.display = "block";
+  button.addEventListener("click", () => {
+    pushEventToDataLayer({
+      event: "experimentStarted",
+      category: "experiments",
+      subCategory: "googleOptimize",
+      eventType: "eventTracking",
+      experimentId: "FdedfcvFRxyZ_syoHheFeA",
+      experimentVariantId: "2",
+      experimentName: "QA Test",
+      experimentVariantName: "Plaid",
+    });
+  });
+};
+
+(() => {
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log(
+      "=== gtm-datalayer-events => Adding braze event handlers for accountLinking"
+    );
+    billboardEvents();
+    experimentTriggerVar1();
+    experimentTriggerVar2();
+    experimentStartedVar1();
+    experimentStartedVar2();
+  });
+})();
